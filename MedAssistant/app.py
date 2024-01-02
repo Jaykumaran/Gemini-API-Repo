6c7b35364198eecef5282fbb0928c1a29ece1ba1
Jaykumaran R <60032500+Jaykumaran@users.noreply.github.com>
2024-01-02 18:34:21 +0530

    Med Assistant

---
+++ MedAssistant/app.py
@@ -0,0 +1,79 @@
+# Q&A Chatbot
+#from langchain.llms import OpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+from PIL import Image
+
+
+import google.generativeai as genai
+
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(input,image,prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Med Assistant ")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+input_prompt = """
+                You are a medical practictioner and an expert in analzying medical related images working for a very reputed hospital.
+                You will be provided with images and you need to identify the anomalies, any disease or health issues. 
+                You need to generate the result in detailed manner. Write all the findings, next steps, recommendation, etc.
+                You only need to respond if the image is related to a human body and health issues.
+                You must have to answer but also write a disclaimer saying that "Consult with a Doctor before making any decisions".
+
+                Remember, if certain aspects are not clear from the image, it's okay to state 'Unable to determine based on the provided image.'
+
+                Now analyze the image and answer the above questions in the same structured manner defined above."
+               """
+
+## If ask button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
